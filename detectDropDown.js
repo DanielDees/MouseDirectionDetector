40f5adbef6144ff2aeb61ff4fe2627545565c9d0
DanielDees <explorers2010@hotmail.com>
2017-04-12 16:03:25 -0400

    Added Dropdown for Services

--- detectDropDown.js
+++ detectDropDown.js
@@ -1,7 +1,7 @@
 ;"use strict";
 
 var map = new posMapper();
-map.init('id', 'nav').setMaxAngle(45).setMaxStopTime(350).setDirection('R').setCallback(function() { 
+map.init('id', 'nav').setMaxAngle(50).setMaxStopTime(350).setDirection('R').setCallback(function() { 
 	if(this.validMoveAngle) {
 		console.log("Check if a:hover and cancel if needed"); 
 	}
